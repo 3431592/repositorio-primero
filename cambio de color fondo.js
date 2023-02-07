5c9153095a5b300050d9769ea4994bfcf25f2002
emicis <emiok89@gmail.com>
2023-02-07 00:50:12 -0300

    primercommit

---
+++ cambio de color fondo.js
@@ -0,0 +1,11 @@
+document.getElementById("boton1").onclick = function () {
+    console.log("capturar")
+    document.getElementById("demo").style.backgroundColor= "lightblue"
+    
+}
+
+document.getElementById("boton2").onclick = function () {
+    console.log("capturar")
+    document.getElementById("demo").style.backgroundColor= "antiquewhite"
+
+}
